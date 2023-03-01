a40b2a180d73f02c4d3d1409abeff466c033b38d
HaiPhan <haithanhphan163@gmail.com>
2023-03-01 21:20:30 +0700

    init

---
+++ src/main/java/com/example/springboot1/model/Employee.java
@@ -0,0 +1,70 @@
+package com.example.springboot1.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+
+    private Long id;
+    @Length(min=3,max=30)
+    private String name;
+    @Min(18)
+    @Max(55)
+    private int age;
+
+    @NotEmpty(message="Image cannot be empty")
+    private String image;
+
+    private String address;
+
+    //auto-generated getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+
+
---
+++ src/main/java/com/example/springboot1/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot1.repository;
+
+
+import com.example.springboot1.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
